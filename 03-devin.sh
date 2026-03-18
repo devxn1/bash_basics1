7cd4ee4cfbaa862d720f3fb0bd73b2e53a5f55d4
devxn <dgomez12@ggc.edu>
2026-03-18 12:13:21 -0400

    Add custom bash script with my name

---
+++ 03-devin.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+echo "Welcome, this script was edit by Devin"
+
+echo "You are happy?"
+read answer
+
+if [ "$answer" = "yes" ]; then
+   echo "Smile :)"
+else
+   echo "Still Smile :)"
+fi
+
+# here are the other string comparison operators
+# != , -n (not an empty string) , -z (an empty string)
+
+# exercise: write a script that prints whether today is
+# the weekend or not
+echo "Which day of a week is today?"
+read day
+if [[ ${day,,} == "saturday" ]] | [[ ${day,,} == "sunday" ]]
+then 
+   echo "Horayyyy!! Today is the weekend. Enjoy yourself! Be ready for the next week."
+else 
+   echo "Today is a weekday. Keep focus on your work."
+fi
